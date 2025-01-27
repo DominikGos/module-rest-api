9277aa4f07a5775701d45e3dd7de59ba4115116c
DominikGos <dominikgos3@gmail.com>
2025-01-28 00:24:19 +0100

    Added mysql env

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-# Set proper ownership and permissions for storage and cache
 echo "Setting permissions for storage and bootstrap/cache directories..."
 chown -R www-data:www-data /var/www/html/storage /var/www/html/bootstrap/cache
 chmod -R 775 /var/www/html/storage /var/www/html/bootstrap/cache
